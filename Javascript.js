1a1bc766b0fb34cb91ada50b926bad3805612ef0
dellpooh47 <shoboon47@gmail.com>
2019-05-21 21:01:20 +0900

    JavaScriptの例追加

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+-- JavaScriptの例
+<script type="text/javascript">
+<!--
+myTbl     = new Array("日","月","火","水","木","金","土");
+myD       = new Date();
+myYear    = myD.getFullYear();
+myMonth   = myD.getMonth() + 1;
+myDate    = myD.getDate();
+myDay     = myD.getDay();
+myHours   = myD.getHours();
+myMinutes = myD.getMinutes();
+mySeconds = myD.getSeconds();
+
